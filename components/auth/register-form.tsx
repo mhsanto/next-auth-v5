29f5a1adf835c9739e3d234d473da5bd08324e03
Mahmodul Hasan <nicesanto92@gmail.com>
2024-01-23 18:41:44 +0600

    added Prisma and Postgresql and created Account User Schema

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -19,7 +19,7 @@ import { RegisterFormSchema } from "@/schemas";
 import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
 import { useState, useTransition } from "react";
-import { login } from "@/action/login.action";
+import { register } from "@/action/login.action";
 export const RegisterForm = () => {
   const [isPending, startTransition] = useTransition();
   const [isSuccess, setIsSuccess] = useState<string | undefined>("");
@@ -38,7 +38,7 @@ export const RegisterForm = () => {
     setIsError("");
     setIsSuccess("");
     startTransition(() => {
-      login(values).then((data) => {
+      register(values).then((data) => {
         setIsSuccess(data?.success);
         setIsError(data?.error);
       });
