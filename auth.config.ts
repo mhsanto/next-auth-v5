26316f48200cbe0244a54028e957adf7e620be91
Mahmodul Hasan <nicesanto92@gmail.com>
2024-01-24 16:51:54 +0600

    Login functionality added a user can now sign-in after create an account

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,22 @@
-import GitHub from "next-auth/providers/github"
-
-import type { NextAuthConfig } from "next-auth"
-
+import bcrypt from "bcryptjs";
+import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { LoginFormSchema } from "./schemas";
+import { getUserByEmail } from "./data/user";
 export default {
-  providers: [GitHub],
-} satisfies NextAuthConfig
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const isValidatedFields = LoginFormSchema.safeParse(credentials);
+        if (isValidatedFields.success) {
+          const { email, password } = isValidatedFields.data;
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) {return null}
+          const isPasswordMatch = await bcrypt.compare(password, user.password);
+          if (isPasswordMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
--- lib/action/login.action.ts
+++ lib/action/login.action.ts
@@ -1,16 +1,31 @@
 "use server";
 import { LoginFormSchema } from "@/schemas";
 import * as z from "zod";
-
+import { AuthError } from "next-auth";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 export const login = async (values: z.infer<typeof LoginFormSchema>) => {
   try {
     const validatedFields = LoginFormSchema.safeParse(values);
+
     if (!validatedFields.success) {
       return { error: "Invalid fields" };
     }
-    return { success: "Email Sent" };
+    const { email, password } = validatedFields.data;
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
   } catch (error) {
-    console.log(error);
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials!" };
+        default:
+          return { error: "Something went wrong" };
+      }
+    }
+    throw error;
   }
 };
-
