19206f931213d4a25373930415009692921d4e0f
Mahmodul Hasan <nicesanto92@gmail.com>
2024-01-25 22:57:32 +0600

    Fixed similar account error where google and github have a same account it shows next-auth sign in page

--- auth.ts
+++ auth.ts
@@ -19,6 +19,18 @@ export const {
   signOut,
 } = NextAuth({
   adapter: PrismaAdapter(db),
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
   callbacks: {
     // @ts-ignore
     async session({ session, token }) {
